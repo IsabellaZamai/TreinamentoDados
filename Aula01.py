b33b79bae7f6ac0dfcb9541a983237a1f9e00c74
IsabellaZamai <bellazamai10@gmail.com>
2026-07-02 16:00:55 -0300

    TreinamentoDADOS

---
+++ Aula01.py
@@ -0,0 +1,21 @@
+#o comando print exibe uma informação na tela do usuario
+#print("Hello World!")
+#print("iniciando treinamento de Análize de Dados")
+#print("30")#idade do usuario
+
+idade = 30 #idade 1 
+idade2 = "30" #idade 2
+
+
+print(idade)
+print(idade2)
+
+
+
+idade_futura = idade + 1
+print(idade_futura)
+
+
+
+print(type(idade))
+print(type(idade2))
